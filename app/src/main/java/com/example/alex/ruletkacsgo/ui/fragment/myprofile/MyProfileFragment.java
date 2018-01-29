6dcc34a4a8e2ccb7c59b7bb52a78cb9f289ccde8
Arthur Yatsun <ayatsun@ukr.net>
2018-01-29 02:14:19 +0200

    Profile 1.0

--- app/src/main/java/com/example/alex/ruletkacsgo/ui/fragment/myprofile/MyProfileFragment.java
+++ app/src/main/java/com/example/alex/ruletkacsgo/ui/fragment/myprofile/MyProfileFragment.java
@@ -17,7 +17,7 @@ public class MyProfileFragment extends android.support.v4.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.roullet, container, false);
+        return inflater.inflate(R.layout.profile, container, false);
     }
 
     @Override
