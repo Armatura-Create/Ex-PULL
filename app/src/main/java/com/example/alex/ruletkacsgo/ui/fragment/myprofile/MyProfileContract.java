4490e637c9e0338ca16903682f44d9890d0aa3dc
Arthur Yatsuna <arthuryatsun@gmail.com>
2018-02-01 19:13:37 +0200

    Settings Button

---
+++ app/src/main/java/com/example/alex/ruletkacsgo/ui/fragment/myprofile/MyProfileContract.java
@@ -0,0 +1,11 @@
+package com.example.alex.ruletkacsgo.ui.fragment.myprofile;
+
+import android.widget.Button;
+
+/**
+ * Created by arthur on 01.02.18.
+ */
+
+public interface MyProfileContract {
+    void onButtonClick(Button button);
+}
--- app/src/main/java/com/example/alex/ruletkacsgo/ui/fragment/myprofile/MyProfileFragment.java
+++ app/src/main/java/com/example/alex/ruletkacsgo/ui/fragment/myprofile/MyProfileFragment.java
@@ -1,23 +1,39 @@
 package com.example.alex.ruletkacsgo.ui.fragment.myprofile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.alex.ruletkacsgo.R;
+import com.example.alex.ruletkacsgo.ui.activity.settings.SettingsActivity;
 
 /**
  * Created by alex on 25.01.18.
  */
 
-public class MyProfileFragment extends android.support.v4.app.Fragment {
+public class MyProfileFragment extends android.support.v4.app.Fragment implements MyProfileContract{
+
+    private Button button;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile, container, false);
+        View view = inflater.inflate(R.layout.profile, container, false);
+
+        button = view.findViewById(R.id.btn_settings);
+
+//        MyProfilePresenter presenter = new MyProfilePresenter();
+//        presenter.onButtonClick(button);
+
+        button.setOnClickListener(view1 -> {
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
+        });
+
+        return view;
     }
 
     @Override
@@ -27,6 +43,11 @@ public class MyProfileFragment extends android.support.v4.app.Fragment {
 
     public static MyProfileFragment newInstance() {
         return new MyProfileFragment();
+    }
+
+    @Override
+    public void onButtonClick(Button button) {
+
     }
 }
 
